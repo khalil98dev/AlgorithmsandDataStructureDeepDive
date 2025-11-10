3f54cb53d60ec144ebc32b85df85922676e05562
khalil98dev <khalil98dev@gmail.com>
2025-11-10 16:58:38 +0100

    Updated II

---
+++ Alogirthms/Algo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Algo;
+
+int[] koko =
+{
+    10,5,3,2,7,98,15,17,19,33
+};
+
+
+Console.WriteLine("before sort: ");
+
+koko.Print();
+
+var Sorted = koko.SortDesc();
+
+Console.WriteLine("After sort Desc: ");
+
+Sorted.Print();
+
+
+var Sorted2 = koko.SortAsc();
+
+Console.WriteLine("After sort Asc: ");
+
+Sorted2.Print();
+
+
+
+Console.WriteLine($"lenght {koko.Length}");
+
+
---
+++ Alogirthms/Algo/SelectedSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo;
+
+public static class SelectedSort
+{
+    public static int[] SortDesc(this int[] result)
+    {
+        //Here => /-\_/-\_/ => 
+
+        int lenght = result.Length;
+
+
+        for (int i = 0; i < lenght; i++)
+        {
+            int indexMinValue = i; 
+
+            for (int j =i+1; j < lenght; j++)
+            {
+                if (result[j] < result[indexMinValue])
+                    indexMinValue = j;    
+            }
+
+            //swap 
+            (result[i], result[indexMinValue]) = (result[indexMinValue], result[i]);
+
+
+        }
+
+
+        return result;
+
+    }
+
+    public static int[] SortAsc(this int[] result)
+    {
+        //Here => /-\_/-\_/ => 
+
+        int lenght = result.Length;
+
+
+        for (int i = 0; i < lenght; i++)
+        {
+            int indexMaxValue = i;
+
+            for (int j = i + 1; j < lenght; j++)
+            {
+                if (result[j] > result[indexMaxValue])
+                    indexMaxValue = j;
+            }
+
+            //swap 
+            (result[i], result[indexMaxValue]) = (result[indexMaxValue], result[i]);
+
+
+        }
+
+
+        return result;
+
+    }
+
+
+    public static void Print(this int[] result)
+    {
+        for (int i=0;i<result.Length;i++)
+        {
+            Console.WriteLine($"Element [{i}]: {result[i]}");
+        }
+
+    }
+
+}
